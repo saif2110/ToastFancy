764fc0a79dc1e0d05c61cdc2f52ac1a7c6e32f35
Saif <gameboy1234@protonmail.com>
2020-02-10 17:10:12 +0530

    Blank

---
+++ Example/Pods/Target Support Files/Pods-ToastFancy_Example/Pods-ToastFancy_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ToastFancy_Example : NSObject
+@end
+@implementation PodsDummy_Pods_ToastFancy_Example
+@end
---
+++ Example/Pods/Target Support Files/ToastFancy/ToastFancy-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ToastFancy : NSObject
+@end
+@implementation PodsDummy_ToastFancy
+@end
