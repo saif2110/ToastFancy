e64115e11afd6ea1f32c934a59aab501ffcd6f48
Saif <gameboy1234@protonmail.com>
2020-02-11 16:14:29 +0530

    ReadME

--- Example/ToastFancy/ViewController.swift
+++ Example/ToastFancy/ViewController.swift
@@ -7,18 +7,37 @@
 //
 
 import UIKit
+import ToastFancy
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    var Position:Int = 0
+    @IBAction func segment(_ sender: UISegmentedControl) {
+        
+        
+        
+        if sender.selectedSegmentIndex == 0 {
+            Position = 0
+        }else if sender.selectedSegmentIndex == 1 {
+            Position = 1
+        }else{
+            Position = 2
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func toastWithImage(_ sender: Any) {
+        
+        ToastFancy.show(view: self.view, text: "\n             \n \n", textColour: .blue, position: .bottom, backgroundImagetoToast: #imageLiteral(resourceName: "so"))
+        
     }
-
+    @IBAction func toast(_ sender: Any) {
+        if Position == 0 {
+            ToastFancy.show(view: view, text: "This is The Toast from ToastFancy 😀",backgroundColor: .purple,position: .top,borderColor: .lightGray,borderWidth: 1)
+        }else if Position == 1 {
+            ToastFancy.show(view: view, text: "This is The Toast from ToastFancy 😀",backgroundColor: .purple,position: .mid,borderColor: .lightGray,borderWidth: 1)
+        }else{
+            ToastFancy.show(view: view, text: "This is The Toast from ToastFancy 😀",backgroundColor: .purple,position: .bottom,borderColor: .lightGray,borderWidth: 1)
+        }
+        
+    }
 }
 
--- README.md
+++ README.md
@@ -11,6 +11,8 @@ To run the example project, clone the repo, and run `pod install` from the Examp
 
 ## Requirements
 
+iOS 11 or above and swift 4 or above
+
 ## Installation
 
 ToastFancy is available through [CocoaPods](https://cocoapods.org). To install
@@ -20,9 +22,28 @@ it, simply add the following line to your Podfile:
 pod 'ToastFancy'
 ```
 
+![sample gif](https://media.giphy.com/media/cnEY3vieuGbMjNToJw/giphy.gif)
+
+
+## How To Use ?
+
+Parameters  are :
+
+* **view**: The view in which the toast is going to be presented
+* **text**: The message that the toast will show
+* **textColour**: The text color of the message (default is white)
+* **position**: The place of toast on view (default is bottom)
+* **backgroundColor**: The background color of the toast (default is black)
+* **borderWidth**: The border width of the toast (default is 0)
+* **borderColor**: The border color of the toast  (default is black)
+* **durationTimeOut**: The time in which toast disappear (default is 2 sec)
+* **backgroundImagetoToast**: The background Image to toast (default is nil)
+
+
+
 ## Author
 
-saif2110, saifmukadam2110@gmail.com
+Saif Muakadam , saifmukadam2110@gmail.com
 
 ## License
 
